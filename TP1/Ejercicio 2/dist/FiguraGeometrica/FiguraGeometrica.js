fd8a312f2be23fd057120b553524f04299dd0e0d
marcosborri <marcosborri06@gmail.com>
2025-09-02 15:03:27 -0300

    Ejercicio 2 hecho

---
+++ TP1/Ejercicio 2/dist/FiguraGeometrica/FiguraGeometrica.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FiguraGeometrica = void 0;
+class FiguraGeometrica {
+    constructor(nombre) {
+        this.nombre = nombre;
+        this.nombre = nombre;
+    }
+}
+exports.FiguraGeometrica = FiguraGeometrica;
