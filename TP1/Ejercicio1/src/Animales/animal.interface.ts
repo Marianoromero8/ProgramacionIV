24eb08cfdc4d7d095a3073f13494daeff2af4fe1
Chulo096 <1lucho2cavero3@gmail.com>
2025-09-02 15:08:19 -0300

    Ejercicio 1 Hecho

---
+++ TP1/Ejercicio1/src/Animales/animal.interface.ts
@@ -0,0 +1,6 @@
+export interface Animal{
+    hacerSonido(): void
+        
+    moverse(): void
+
+}
---
+++ TP1/Ejercicio1/src/index.ts
@@ -0,0 +1,5 @@
+import {Perro} from "./Animales/perro"
+
+const p1 = new Perro
+p1.hacerSonido()
+p1.moverse()
