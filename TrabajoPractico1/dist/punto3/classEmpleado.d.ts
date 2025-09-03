de50f1d97b65ed6c7c12b88c69b4aad44f325325
marianoromero8 <marianoromero97.mr@gmail.com>
2025-09-03 16:33:27 -0300

    .gitignore add

--- TrabajoPractico1/dist/punto3/classEmpleado.d.ts
+++
@@ -1,7 +0,0 @@
-export declare abstract class Empleado {
-    nombre: string;
-    salarioBase: number;
-    constructor(nombre: string, salarioBase: number);
-    abstract calcularSalario(): void;
-}
-//# sourceMappingURL=classEmpleado.d.ts.map
--- TrabajoPractico1/dist/punto3/classEmpleadoTiempoCompleto.d.ts
+++
@@ -1,6 +0,0 @@
-import { Empleado } from "./classEmpleado";
-export declare class EmpleadoTiempoCompleto extends Empleado {
-    constructor(nombre: string, salarioBase: number);
-    calcularSalario(): number;
-}
-//# sourceMappingURL=classEmpleadoTiempoCompleto.d.ts.map
