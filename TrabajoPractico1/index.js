0ade74d610f3ca39ace5c09bf17e8ae2b749f8fc
FrancoPanzone <fjpanzone@gmail.com>
2025-09-03 20:38:57 -0300

    Unico Main.ts y config

--- TrabajoPractico1/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const punto3_1 = require("./src/punto3");
-(0, punto3_1.puntoTres)();
-//# sourceMappingURL=index.js.map
