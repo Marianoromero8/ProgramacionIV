5c07d3772925bc6d970908e8d3cefa1c344ca496
marianoromero8 <marianoromero97.mr@gmail.com>
2025-11-16 21:01:58 -0300

    SQL Injection - 7: Hecho

--- Practico Ciberseguridad/WebApp-Seguridad-Prog4/backend/src/controllers/productController.js
+++ Practico Ciberseguridad/WebApp-Seguridad-Prog4/backend/src/controllers/productController.js
@@ -1,21 +1,27 @@
-const { db } = require('../config/database');
+const { db } = require("../config/database");
 
 // VULNERABLE: SQL Injection
 const getProducts = (req, res) => {
   const { category, search } = req.query;
-  
+  const regexInjection = /['";\-]/;
+  if (regexInjection.test(category) || regexInjection.test(search)) {
+    return res.json([]);
+  }
   // VULNERABLE: Concatenación directa de strings en SQL
-  let query = 'SELECT * FROM products WHERE 1=1';
-  
+  let query = "SELECT * FROM products WHERE 1=1";
+  let params = [];
+
   if (category) {
-    query += ` AND category = '${category}'`;
+    query += ` AND category = ?`;
+    params.push(category);
   }
-  
+
   if (search) {
-    query += ` AND name LIKE '%${search}%'`;
+    query += "AND name LIKE ?";
+    params.push(`%${search}%`);
   }
-  
-  db.query(query, (err, results) => {
+
+  db.query(query, params, (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
@@ -24,5 +30,5 @@ const getProducts = (req, res) => {
 };
 
 module.exports = {
-  getProducts
+  getProducts,
 };
